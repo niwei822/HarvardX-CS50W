c0fef29cc2a98b7fa88d5d99a634220d1b96c6cb
niwei822 <cecily.li.822@gmail.com>
2023-03-25 23:05:18 -0700

    add test file

--- learn_django/airline/flights/models.py
+++ learn_django/airline/flights/models.py
@@ -20,7 +20,10 @@ class Flight(models.Model):
     duration = models.IntegerField()
     
     def __str__(self):
-        return f"{self.id}: {self.origin} to {self.destination}"
+        return f"{self.origin} to {self.destination}"
+    
+    def is_valid_flight(self):
+        return self.origin != self.destination and self.duration > 0
 
 class Passenger(models.Model):
     first = models.CharField(max_length=64)
